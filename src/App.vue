420230f59ab16b12f3799adb0f35fb77eb52d3e7
Cancey Dejean <canceyd@gmail.com>
2024-10-14 13:11:41 -0500

    added starter vue files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="container py-8">
+    <h1 class="text-4xl">Hello</h1>
+    <Button>Click me</Button>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
